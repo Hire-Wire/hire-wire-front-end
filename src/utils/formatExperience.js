d74622a4d9bbd3524374be70b5b3d9b5e0532bef
austenzhang <169107245+austenzhang@users.noreply.github.com>
2024-11-18 20:21:59 -0700

    Update formatExperience.js

--- src/utils/formatExperience.js
+++ src/utils/formatExperience.js
@@ -11,7 +11,7 @@ const formatExperience = (experiences) => {
       jobTitle: employment.jobTitle,
       organizationName,
       startDate: employment.startDate.split("T")[0],
-      endDate: employment.endDate.split("T")[0],
+      endDate: employment.endDate ? employment.endDate.split("T")[0] : "", // Handle null
       jobDescription: employment.jobDescription,
     }));
 
@@ -20,7 +20,7 @@ const formatExperience = (experiences) => {
       id: education.id,
       organizationName,
       startDate: education.startDate.split("T")[0],
-      endDate: education.endDate.split("T")[0],
+      endDate: education.endDate ? education.endDate.split("T")[0] : "", // Handle null
       fieldOfStudy: education.fieldOfStudy,
       grade: `${education.grade}/4.0`,
       degree: education.degree,
